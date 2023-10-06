6d35179f690afe2e9d921b33cbb4c7817404f1c1
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-10-06 16:45:54 +0100

    fix linters

--- person.rb
+++ person.rb
@@ -1,24 +1,25 @@
 class Person
+  # getter and setter in one
+  attr_accessor :name, :age
+
+
+  # getter
+  attr_reader :id
+
   def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..100)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
-  # getter and setter in one
-  attr_accessor :name
-  attr_accessor :age
-
-  # getter
-  attr_reader :id
 
   def of_age?
     @age >= 18
   end
-  private :is_of_age?
+  private :of_age?
 
   def can_use_services?
-    @parent_permission || is_of_age?
+    @parent_permission || of_age?
   end
 end
 
